c408ee32372935b0da134118dea63eb0ec3b4e87
Rakin <rakin92@gmail.com>
2021-11-08 02:17:32 -0500

    updated test scripts

--- scripts/run-test.sh
+++ scripts/run-test.sh
@@ -5,7 +5,7 @@ program="$buildPath/$app"
 printf "\nStart testing: $app\n"
 # Set all ENV vars for the server to run
 export $(grep -v '^#' .env | xargs)
-time go test ./...
+time go test ./... -race -coverprofile=coverage.txt -covermode=atomic
 # This should unset all the ENV vars, just in case.
 unset $(grep -v '^#' .env | sed -E 's/(.*)=.*/\1/''' | xargs)
 printf "\nRun tests completed: $app\n\n"
